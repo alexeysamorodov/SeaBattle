7640946e4664c0684e960891732c628832c61229
samorodov <alexeysamorodov@gmail.com>
2021-03-11 13:53:05 +0500

    класс для статистики битвы

---
+++ SeaBattle/Data/BattleStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SeaBattle.Data
+{
+    public class BattleStatistics
+    {
+        [JsonPropertyName("ship_count")]
+        public int ShipCount { get; set; } = 0;
+
+        [JsonPropertyName("destroyed")]
+        public int DestroyedCount { get; set; } = 0;
+
+        [JsonPropertyName("knocked")]
+        public int KnockedCount { get; set; } = 0;
+
+        [JsonPropertyName("shot_count")]
+        public int ShotCount { get; set; } = 0;
+    }
+}
