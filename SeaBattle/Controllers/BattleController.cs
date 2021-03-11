656c89ae92b977718c865179b27e01adf95ce051
samorodov <alexeysamorodov@gmail.com>
2021-03-11 14:00:37 +0500

    заготовка для основного контроллера

---
+++ SeaBattle/Controllers/BattleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeaBattle.Data;
+
+namespace SeaBattle.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BattleController : ControllerBase
+    {
+        [Route("create-matrix")]
+        [HttpPost]
+        public void CreateMatrix()
+        {
+
+        }
+
+        [Route("ship")]
+        [HttpPost]
+        public void CreateShip()
+        {
+
+        }
+
+        [Route("shot")]
+        [HttpPost]
+        public void TakeShot()
+        {
+
+        }
+
+        [Route("clear")]
+        [HttpPost]
+        public void ClearBattle()
+        {
+
+        }
+
+        [Route("state")]
+        [HttpGet]
+        public BattleStatistics GetStatistics()
+        {
+            return new BattleStatistics();
+        }
+    }
+}
