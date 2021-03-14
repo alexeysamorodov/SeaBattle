1c535b6792a462bba2c9111603681772c9cbc7cf
samorodov <alexeysamorodov@gmail.com>
2021-03-14 20:36:00 +0500

    проект для тестов

---
+++ SeaBattle.Tests/CoordinatesParserTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SeaBattle.Tests
+{
+    public class CoordinatesParserTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
