5f7abc0fe79ead1438f75fc2f0f59736fa02e03c
samorodov <alexeysamorodov@gmail.com>
2021-03-15 10:55:40 +0500

    custom исключения для парсинга координат

---
+++ SeaBattle/Exceptions/BadCoordinatesException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SeaBattle.Exceptions
+{
+    public class BadCoordinatesException: Exception
+    {
+        protected string BadCoordinates;
+        public BadCoordinatesException(string badStringCoordinates)
+        {
+            BadCoordinates = badStringCoordinates;
+        }
+
+        public override string ToString()
+        {
+            return $"Invalid coordinates: {BadCoordinates}";
+        }
+    }
+}
---
+++ SeaBattle/Exceptions/ShipIntersectionException.cs
@@ -0,0 +1,15 @@
+﻿namespace SeaBattle.Exceptions
+{
+    public class ShipIntersectionException : BadCoordinatesException
+    {
+        public ShipIntersectionException(string badStringCoordinates) 
+            : base(badStringCoordinates)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Ship with coordinates: {BadCoordinates} intersects previous ships";
+        }
+    }
+}
